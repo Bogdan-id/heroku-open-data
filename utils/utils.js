2a05a226effc7d920034cb16a80fc590579533c9
Bogdan-t <user332145@gmail.com>
2020-11-05 16:17:35 +0200

    added new req origins

--- utils/utils.js
+++ utils/utils.js
@@ -8,8 +8,10 @@ exports.setHeaderToOrigin = function (res, req, next) {
   let allowOrigin = [
     'http://localhost:8080',
     'https://fervent-agnesi-d2138f.netlify.app',
-    'https://agent.bestleasing.com.ua/',
-    'http://127.0.0.1:8001/'
+    'https://agent.bestleasing.com.ua',
+    'http://127.0.0.1:8001',
+    'http://localhost:8001',
+    'http://localhost:8000'
   ];
 
   if(allowOrigin.indexOf(origin) > -1) {
