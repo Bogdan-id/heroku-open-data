47597613b075a7164e93da62ce528a0d174d9830
Bogdan-t <user332145@gmail.com>
2021-01-04 15:46:53 +0200

    added new origin to requests

--- utils/utils.js
+++ utils/utils.js
@@ -10,6 +10,7 @@ exports.setHeaderToOrigin = function (res, req, next) {
     'https://fervent-agnesi-d2138f.netlify.app',
     'https://agent.bestleasing.com.ua',
     'http://127.0.0.1:8001',
+    'http://127.0.0.1:8000',
     'http://localhost:8001',
     'http://localhost:8000'
   ];
