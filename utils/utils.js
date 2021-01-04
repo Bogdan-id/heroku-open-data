61d4543f57fe27ebd9292ea81f07e6b2a341cd81
Bogdan-t <user332145@gmail.com>
2021-01-04 16:12:16 +0200

    disable strict origin

--- utils/utils.js
+++ utils/utils.js
@@ -17,12 +17,12 @@ exports.setHeaderToOrigin = function (res, req, next) {
     'http://localhost'
   ];
 
-  if(allowOrigin.indexOf(origin) > -1) {
+  // if(allowOrigin.indexOf(origin) > -1) {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-  } 
+  // } 
 
   next()
 }
