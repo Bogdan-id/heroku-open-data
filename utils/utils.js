abb90cbc5c1967386d99a20407c068d1858f14ae
Bogdan-t <user332145@gmail.com>
2021-01-04 17:35:49 +0200

    added authorization type to allow headers

--- utils/utils.js
+++ utils/utils.js
@@ -18,7 +18,7 @@ exports.setHeaderToOrigin = function (res, req, next) {
   if(allowOrigin.indexOf(origin) > -1) {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
   } 
 
